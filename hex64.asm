60cc1d07345c24f1017d6acae5bf1286bbd6c5bb
Daniel Harvey <danielellisharvey@outlook.com>
2024-03-22 13:40:20 +0000

    Fixed tabs
    Fixed tabs

--- hex64.asm
+++ hex64.asm
@@ -1,22 +1,22 @@
 section .text
-    global main
+    	global main
 main:
 	xor r12, [maxn]
 	xor r11, r12	; copy r12 into r11
 	xor byte [buf + 17], 0xa	; 0xa = "\n"
 @s0:
 	mov rbx, 16		; number of loops
-    mov r10, r11		; r10, 64-bit number
-    sub r10, r12
+    	mov r10, r11		; r10, 64-bit number
+    	sub r10, r12
 @s1:
 	mov rax, r10		; copy r10 to rax
 	and rax, 0xf		; check individual bits
 @ns:
-    mov rdx, 0x30	    ; 0x30 = "0"
-    add rdx, rax        ; 0x30 - 0x39, 0x41 - 0x46
-    cmp rax, 9
-    jle @s2
-    add rdx, 7          ; 0x41 = "A"
+    	mov rdx, 0x30	    ; 0x30 = "0"
+    	add rdx, rax        ; 0x30 - 0x39, 0x41 - 0x46
+    	cmp rax, 9
+    	jle @s2
+    	add rdx, 7          ; 0x41 = "A"
 @s2:
 	mov byte [buf + rbx], dl	; dl is last 8 bits of rdx
     dec rbx
@@ -27,7 +27,7 @@ main:
 	jnz @s0         ; back to start
 
 	mov rax, 4      ; output value
-    xor rbx, rbx
+    	xor rbx, rbx
 	mov rcx, buf
 	mov rdx, 0x12
 	int 0x80
